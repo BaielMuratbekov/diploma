28cf93af20bea4a59cba354c976ab9cf0bb3a2d0
BaielMuratbekov <bajelmuratbekov044@gmail.com>
2022-04-08 20:32:49 +0600

    delet all the files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import Card from "./components/Card/Card";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Nav from "./components/Nav/Nav";
+
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Header/>
-      <Card/>
-      <Footer/>
+
     </div>
   );
 }
