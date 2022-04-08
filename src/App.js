73613e1cae6d3e8e367ca183e7b82d63ea132a81
BaielMuratbekov <bajelmuratbekov044@gmail.com>
2022-04-08 23:13:06 +0600

    add assest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Header } from "./components/Header";
 
-
+import { HomePage } from './components/Pages/Home-page'
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
 
-    </div>
+      </div>
+    </BrowserRouter >
   );
 }
 
