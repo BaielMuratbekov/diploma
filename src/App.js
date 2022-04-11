2d74bbbef9a5d11928cfc162351de7d082c87f40
BaielMuratbekov <bajelmuratbekov044@gmail.com>
2022-04-11 15:19:51 +0600

    add react-router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import {
   Route,
 } from "react-router-dom";
 import { Header } from "./components/Header";
+import { Nav } from "./components/Header/Nav";
+import { Contact } from "./components/Pages/Contact";
 
 import { HomePage } from './components/Pages/Home-page'
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header/>
+        <Header />
+        <Nav />
         <Routes>
           <Route index element={<HomePage />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
 
       </div>
--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Nav } from '../../Header/Nav'
 
 export const Contact = () => {
     return (
