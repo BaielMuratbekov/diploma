156ad3bba836c9e272acc913e5727a1cd79005a6
BaielMuratbekov <bajelmuratbekov044@gmail.com>
2022-04-11 15:19:29 +0600

    New components Home-page

---
+++ src/components/Pages/Home-page/Home-page.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { FootballItem } from '../../Football/FootballItem'
+import "./Home-page.css"
+
+export const HomePage = () => {
+    const FOOTBALLS = [
+        {
+            image: '/assets/realforma1.jpg',
+            title: 'Real Madrid',
+            price: 2343,
+            id: 1,
+            description: "lorem",
+        },
+        {
+            image: '/assets/ManchesterUnited.jpg',
+            title: 'ManchesterUnited',
+            price: 200,
+            id: 1,
+            description: "lorem",
+        },
+        {
+            image: '/assets/realforma1.jpg',
+            title: 'Real Madrid',
+            price: 2343,
+            id: 1,
+            description: "lorem",
+        },
+        {
+            image: '/assets/ManchesterUnited.jpg',
+            title: 'ManchesterUnited',
+            price: 200,
+            id: 1,
+            description: "lorem",
+        },
+        {
+            image: '/assets/realforma1.jpg',
+            title: 'Real Madrid',
+            price: 2343,
+            id: 1,
+            description: "lorem",
+        },
+        {
+            image: '/assets/chelsea.jpg',
+            title: 'Chelsea',
+            price: 200,
+            id: 1,
+            description: "lorem",
+        },
+        {
+            image: '/assets/topsport1.webp',
+            title: 'Real Madrid',
+            price: 2222,
+            id: 1,
+            description: "lorem",
+        },
+        {
+            image: '/assets/ManchesterUnited.jpg',
+            title: 'ManchesterUnited',
+            price: 200,
+            id: 1,
+            description: "lorem",
+        },
+    ]
+    return (
+        <div className='container'>
+            <div className='Home-page'>
+                {FOOTBALLS.map(football =>
+                    <FootballItem football={football}
+                        key={football.id} />)}
+            </div>
+        </div>
+    )
+}
