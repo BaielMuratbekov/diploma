5e685a161d61148f07382d39b912b8ea615f2340
BaielMuratbekov <bajelmuratbekov044@gmail.com>
2022-04-11 15:17:55 +0600

    New components Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Header.css"
+import { Link } from 'react-router-dom'
+import { CartBlok } from '../Cart-blok/Cart-blok.js'
+export const Header = () => {
+    return (
+        <div className='Header'>
+            <div className='wrapper'>
+                <Link to="/" className='header__title'>Footbal Store</Link>
+            </div>
+            <div className='wrapper'>
+                <Link to="/" className='header__link'>Home</Link>
+                <Link to="/" className='header__link'>About</Link>
+                <Link to="/contact" className='header__link'>Contact</Link>
+                <Link to="/" className='header__link'>About</Link>
+            </div>
+            <div className='wrapper header__cart-btn'>
+                <CartBlok/>
+            </div>
+        </div>
+    )
+}
