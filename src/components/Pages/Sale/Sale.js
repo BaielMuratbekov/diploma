9351f2ad85c180ec11f2313beeb20f95f79abb6d
BaielMuratbekov <bajelmuratbekov044@gmail.com>
2022-04-13 15:50:09 +0600

    New components Sale

---
+++ src/components/Pages/Sale/Sale.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Sale.css'
+import { MainHeader } from '../../MainHeader/MainHeader'
+
+export const Sale = () => {
+  return (
+    <div className='Sale'>
+
+      <h1 className='activeRed'>Sale to -50%</h1>
+      <MainHeader/>
+    </div>
+  )
+}
