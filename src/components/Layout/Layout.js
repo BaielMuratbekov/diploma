b7648efd3296a5419ca5a9cb1af65a167e7d8a3c
BaielMuratbekov <bajelmuratbekov044@gmail.com>
2022-04-20 14:59:58 +0600

    test

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './Layout.css'
 import { Footer } from "../Footer";
 import { Nav } from '../Nav/Nav';
-import { NavSecond } from '../Nav/NavSecond/NavSecond';
 export const Layout=({ children })=> {
     return (
         <div className='Layout'>
