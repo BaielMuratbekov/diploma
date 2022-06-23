4a4f72a9681caff2753636ce2d86fbdab2ee2b7e
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-23 11:24:56 +0600

    Update CartBlok

--- src/components/CartBlok/CartBlok.js
+++ src/components/CartBlok/CartBlok.js
@@ -1,29 +1,19 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import "./CartBlok.css"
-import { BiCartAlt } from 'react-icons/bi'
-import { CartMenu } from '../CartMenu/CartMenu'
-import { calcTotalPrice } from '../untils'
-import { ItemsInCart } from '../ItemsInCart/ItemsInCart'
-
+import { useSelector } from "react-redux";
+import "./CartBlok.css";
+import { BsCart2 } from "react-icons/bs";
+import { ItemsInCart } from "../ItemsInCart/ItemsInCart";
+import { NavLink } from "react-router-dom";
 
 export const CartBlok = () => {
-    const [isCartMenuVisible, setIsCartMenuVisible] = useState(false);
-    const items = useSelector((state) => state.cart.itemsInCart);
-    const totalPrice = calcTotalPrice(items);
-
-    return (
-        <div className="cart-block">
-            <ItemsInCart quantity={items.length} />
-            <BiCartAlt
-                size={30}
-                className="cart-icon"
-                onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}
-            />
-            {totalPrice > 0 ? (
-                <span className="total-price">{totalPrice} сом.</span>
-            ) : null}
-            {isCartMenuVisible && <CartMenu onClick={() => null} />}
-        </div>
-    );
+    const items = useSelector(store => {
+        return Object.values(store.cart.itemsInCart).reduce((sum, number) => sum + number, 0);
+      });
+  return (
+    <NavLink to='/cart'>
+      <div className="cart-block">
+        <ItemsInCart quantity={items} />
+        <BsCart2 size={30} className="cart-icon" />
+      </div>
+    </NavLink>
+  );
 };
