b652492b72c4f25d7f525b2eca1d5ad2b570dbd4
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-23 11:24:02 +0600

    Create a Redux store and cartSlice

--- src/components/redux/cart/reducer.js
+++ src/components/redux/cart/reducer.js
@@ -1,18 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
-const cartSlice = createSlice({
-    name: 'cart',
-    initialState: {
-        itemsInCart: []
-    },
-    reducers: {
-        setItemInCart: (state, action) => {
-            state.itemsInCart.push(action.payload)
-        },
-        deletItemFromCart: (state, action) => {
-            state.itemsInCart = state.itemsInCart.filter(football => football.id !== action.payload)
-        }
-    },
 
+function saveInStorage(itemsInCart) {
+  localStorage.setItem("cartItems", JSON.stringify(itemsInCart));
+}
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    itemsInCart: {},
+  },
+  reducers: {
+    add: (store, action) => {
+      if (store.itemsInCart[action.payload]) {
+        store.itemsInCart[action.payload]++;
+      } else {
+        store.itemsInCart[action.payload] = 1;
+      }
+      saveInStorage(store.itemsInCart);
+    },
+    delete: (store, action) => {
+      delete store.itemsInCart[action.payload];
+      saveInStorage(store.itemsInCart);
+    },
+    increment: (store, action) => {
+      store.itemsInCart[action.payload]++;
+      saveInStorage(store.itemsInCart);
+    },
+    decrement: (store, action) => {
+      if (store.itemsInCart[action.payload] > 1) {
+        store.itemsInCart[action.payload]--;
+      } else {
+        delete store.itemsInCart[action.payload];
+      }
+      saveInStorage(store.itemsInCart);
+    },
+    restore: (store, action) => {
+      store.itemsInCart = JSON.parse(localStorage.getItem("cartItems") ?? "{}");
+    },
+  },
 });
-export const { setItemInCart, deletItemFromCart } = cartSlice.actions;
+
 export default cartSlice.reducer;
