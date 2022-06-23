18e784bfa1e2476b7effeb681da8f0adc0657135
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-23 11:25:20 +0600

    Update FootballByu

--- src/components/Football/FootballByu/FootballByu.js
+++ src/components/Football/FootballByu/FootballByu.js
@@ -1,34 +1,20 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Button } from '../../Button'
-import { deletItemFromCart, setItemInCart } from '../../redux/cart/reducer';
-import "./FootballByu.css"
-import { BsFillCartXFill } from 'react-icons/bs'
-import { BsFillCartPlusFill } from 'react-icons/bs'
+import React from "react";
+import { useDispatch } from "react-redux";
+import "./FootballByu.css";
+import { BsFillCartPlusFill } from "react-icons/bs";
 
-export const FootballByu = ({ product }) => {
-    const dispatch = useDispatch();
-    const items = useSelector((state) => state.cart.itemsInCart);
-    const isItemInCart = items.some((item) => item.productId === product.productId);
-    const handleClick = (e) => {
-        e.stopPropagation();
-        if (isItemInCart) {
-            dispatch(deletItemFromCart(product.productId));
-        } else {
-            dispatch(setItemInCart(product));
-        }
-    };
+export const FootballByu = ({ productId }) => {
+  const dispatch = useDispatch();
 
-    return (
-        <div className='Football-byu'>
-            <span className='football-byu__price'>{product.price} сом.</span>
-            <Button
-                type={isItemInCart ? "secondary" : "primary"}
-                onClick={handleClick}
-            >
-                {isItemInCart ? <BsFillCartXFill size={23} color='tomato' /> : <BsFillCartPlusFill color='rgb(255, 255, 255)' size={23}/>}
-            </Button>
+  function onAddToCart() {
+    dispatch({ type: "cart/add", payload: productId });
+  }
 
-        </div>
-    )
-}
+  return (
+    <div className="Football-byu">
+      <button onClick={onAddToCart}>
+        <BsFillCartPlusFill color="rgb(255, 255, 255)" size={50} />
+      </button>
+    </div>
+  );
+};
