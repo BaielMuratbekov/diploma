2c8e3d1665bb3e2ac0f4d1f39751c9826ac78f4f
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-23 11:26:11 +0600

    Update FootballLIst

--- src/components/Football/FootballList/FootballList.js
+++ src/components/Football/FootballList/FootballList.js
@@ -2,10 +2,9 @@ import classes from "./FootballList.module.css";
 import { FootballItem } from "../FootballItem";
 
 function FootballList({ products }) {
-    
     const productItems = products.map(product => (
-        <FootballItem key={product.productId} product={product} />
-      ));
+      <FootballItem key={product.productId} product={product} />
+    ));
     return (
         <div className={classes.FootballList}>
             {productItems}
