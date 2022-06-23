cef69af6a6c09339026f183b6d8d7a8d8f461d33
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-23 11:30:22 +0600

    Fix the productId issue

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,11 +1,13 @@
 import { getProduct } from "../data/products";
 import { Header } from "../components/Header/Header";
 import { useParams } from "react-router-dom";
+import { FootballByu } from "../components/Football/FootballByu/FootballByu";
+
 
 function Product() {
   const params = useParams();
   const product = getProduct(params.productId);
-
+  
   if (!product) {
     return null;
   }
@@ -15,7 +17,9 @@ function Product() {
       <Header title={product.title} image={product.image}>
         {product.description}
       </Header>
+      <FootballByu productId={params.productId}/>
     </>
+    
   );
 }
 
