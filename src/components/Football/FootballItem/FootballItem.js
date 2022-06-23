cedd526a70d5b31b9ea2629b64a424cf5abc89c2
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-23 11:25:50 +0600

    Update FootballItem

--- src/components/Football/FootballItem/FootballItem.js
+++ src/components/Football/FootballItem/FootballItem.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import "./FootballItem.css"
-import { FootballByu } from '../FootballByu/FootballByu';
-import { FootballCover } from '../FootballCover/FootballCover';
-import { Link } from 'react-router-dom';
-export const FootballItem = ({ product  }) => {
-    return (
+import React from "react";
+import "./FootballItem.css";
+import { FootballCover } from "../FootballCover/FootballCover";
+import { Link } from "react-router-dom";
 
-        <div className='FootballItem'>
-            <FootballCover image={product.image} />
-            <div className='footballitem__detalis'>
-                <Link to={"/" + product.productId} className='footballitem__title'>{product.title}</Link>
-                <div className='footballitem__buy'><FootballByu product={product}/></div>
-            </div>
+export const FootballItem = ({ product }) => {
+  return (
+    <div className="FootballItem">
+      <FootballCover image={product.image} />
+      <div className="footballitem__detalis">
+        <Link
+          to={"/products/" + product.productId}
+          className="footballitem__title"
+        >
+          {product.title}
+        </Link>
+        <div className="footballitem__buy">
+          <span className="football-byu__price">{product.price} сом.</span>
         </div>
-
-    );
-
-
-}
+      </div>
+    </div>
+  );
+};
