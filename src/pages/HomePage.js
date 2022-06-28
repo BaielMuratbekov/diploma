eb40e6bcb8ce27c92853ceb326deea3410c00734
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-28 20:06:21 +0600

    update FootballLIst

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import CategoryList from "../components/CategoryList/CategoryList";
+import FootballList from "../components/Football/FootballList/FootballList";
 import { Header } from "../components/Header/Header";
 import { PlayersHeader } from "../components/PlayersHeader/PlayersHeader";
 import { getCategories } from "../data/categories";
+import { getProducts } from "../data/products";
 export const HomePage = () => {
   return (
     <div className="Home-page">
@@ -13,14 +15,21 @@ export const HomePage = () => {
           title="Welcome to our football store!"
           image="/assets/aboutfootball.jpg"
         >
-          A coffee bean is a seed of the Coffea plant and the source for coffee.
-          It is the pip inside the red or purple fruit often referred to as a
-          cherry. Just like ordinary cherries, the coffee fruit is also a
-          so-called stone fruit. Even though the coffee beans are not
-          technically beans, they are referred to as such because of their
-          resemblance to true beans.
+          Football is a family of team sports that involve, to varying degrees,
+          kicking a ball to score a goal. Unqualified, the word football
+          normally means the form of football that is the most popular where the
+          word is used. Sports commonly called football include association
+          football (known as soccer in North America and Oceania); gridiron
+          football (specifically American football or Canadian football);
+          Australian rules football; rugby union and rugby league; and Gaelic
+          football.[1] These various forms of football share to varying extent
+          common origins and are known as football codes.
         </Header>
       </>
+      <div className="container">
+        <h1 className="title">ALL PRODUCTS</h1>
+        <FootballList products={getProducts()} />
+      </div>
     </div>
   );
 };
